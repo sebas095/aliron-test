e0c792dbaaaa93435e38f78c209a8858b8f48fc2
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:31:32 -0500

    [ADD] index routes added

---
+++ api/src/routes/index.routes.js
@@ -0,0 +1,6 @@
+module.exports = {
+  UserRoutes: require("./user.routes"),
+  AuthRoutes: require("./auth.routes"),
+  StageRoutes: require("./stage.routes"),
+  TournamentRoutes: require("./tournament.routes"),
+};
