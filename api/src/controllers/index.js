92078c32ad0eef27c6259c2dd4257aca6154dd1a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:30:20 -0500

    [ADD] index controller added

---
+++ api/src/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  UserController: require("./user.controller"),
+  AuthController: require("./auth.controller"),
+  StageController: require("./stage.controller"),
+  TournamentController: require("./tournament.controller"),
+};
