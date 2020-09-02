6871ae05f14a246dc1beed4547059ea4d79094d0
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:28:57 -0500

    [ADD] config api routes added

---
+++ api/src/routes/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const logger = require("morgan");
+require("express-async-errors");
+
+const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
+
+module.exports = ({
+  UserRoutes,
+  AuthRoutes,
+  TournamentRoutes,
+  StageRoutes,
+}) => {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+
+  // default middlewares
+  apiRoutes
+    .use(express.json())
+    .use(cors())
+    .use(helmet())
+    .use(logger("dev"))
+    .use(compression());
+
+  // apiRoutes.use("/user", UserRoutes);
+  // apiRoutes.use("/auth", AuthRoutes);
+  // apiRoutes.use("/tournament", TournamentRoutes);
+  // apiRoutes.use("/stage", StageRoutes);
+
+  router.use("/v1/api", apiRoutes);
+
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
+
+  return router;
+};
