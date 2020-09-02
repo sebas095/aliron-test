39df3ae52e9a141d920f2aa74594d8396ebdf0ff
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:30:39 -0500

    [ADD] auth service added

---
+++ api/src/services/auth.service.js
@@ -0,0 +1,57 @@
+const { JWTHelper } = require("../helpers");
+let _userService = null;
+
+class AuthService {
+  // dependency injection
+  constructor({ UserService }) {
+    _userService = UserService;
+  }
+
+  async signUp(user) {
+    const { username } = user;
+    const userExist = await _userService.getUserByUsername(username);
+
+    if (userExist) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "User already exist";
+      throw error;
+    }
+
+    return await _userService.create(user);
+  }
+
+  async signIn(user) {
+    const { username, password } = user;
+    const userExist = await _userService.getUserByUsername(username);
+
+    if (!userExist) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "User does not exist";
+      throw error;
+    }
+
+    const validPassword = userExist.comparePasswords(password);
+
+    if (!validPassword) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "Invalid password";
+      throw error;
+    }
+
+    const userToEncode = {
+      id: userExist._id,
+      role: userExist.role,
+      username: userExist.username,
+      status: userExist.status,
+    };
+
+    const token = JWTHelper.generateToken(userToEncode);
+
+    return { token, user: userExist };
+  }
+}
+
+module.exports = AuthService;
