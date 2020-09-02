a7ba22cf9a2dd85549bb5cbe3f1e7c942e8e2a08
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 17:23:31 -0500

    [ADD] footer component added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
-const Footer = () => {
-  return <div></div>;
-};
+const Container = styled.footer`
+  grid-area: footer;
+  background-color: white;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+`;
+
+const Footer = () => (
+  <Container>
+    <div>
+      <span>©{new Date().getFullYear()}&nbsp;</span>
+      Hecho con 🤘
+    </div>
+  </Container>
+);
 
 export default Footer;
