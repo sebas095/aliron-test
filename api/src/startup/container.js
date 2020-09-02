ce1c5ec53e80153d71962196861c4585e96c0468
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:26:41 -0500

    [ADD] dependency injection registered

--- api/src/startup/container.js
+++ api/src/startup/container.js
@@ -6,31 +6,79 @@ const config = require("../config");
 const app = require(".");
 
 // services
-const {} = require("../services");
+const {
+  UserService,
+  AuthService,
+  StageService,
+  TournamentService,
+} = require("../services");
 
 // controllers
-const {} = require("../controllers");
+const {
+  UserController,
+  AuthController,
+  StageController,
+  TournamentController,
+} = require("../controllers");
 
 // routes
-const {} = require("../routes/index.routes");
+const {
+  UserRoutes,
+  AuthRoutes,
+  StageRoutes,
+  TournamentRoutes,
+} = require("../routes/index.routes");
 // router
 const Routes = require("../routes");
 
+// models
+const { User, Stage, Tournament } = require("../models");
+
 // repositories
-const {} = require("../repositories");
+const {
+  UserRepository,
+  StageRepository,
+  TournamentRepository,
+} = require("../repositories");
 
 const container = createContainer();
 
+// register dependency injection
 container
   .register({
     app: asClass(app).singleton(),
     router: asFunction(Routes).singleton(),
     config: asValue(config),
   })
-  .register({})
-  .register({})
-  .register({})
-  .register({})
-  .register({});
+  .register({
+    UserRoutes: asFunction(UserRoutes).singleton(),
+    AuthRoutes: asFunction(AuthRoutes).singleton(),
+    StageRoutes: asFunction(StageRoutes).singleton(),
+    TournamentRoutes: asFunction(TournamentRoutes).singleton(),
+  })
+  .register({
+    UserController: asClass(UserController.bind(UserController)).singleton(),
+    AuthController: asClass(AuthController.bind(AuthController)).singleton(),
+    StageController: asClass(StageController.bind(StageController)).singleton(),
+    TournamentController: asClass(
+      TournamentController.bind(TournamentController)
+    ).singleton(),
+  })
+  .register({
+    UserService: asClass(UserService).singleton(),
+    AuthService: asClass(AuthService).singleton(),
+    StageService: asClass(StageService).singleton(),
+    TournamentService: asClass(TournamentService).singleton(),
+  })
+  .register({
+    UserRepository: asClass(UserRepository).singleton(),
+    StageRepository: asClass(StageRepository).singleton(),
+    TournamentRepository: asClass(TournamentRepository).singleton(),
+  })
+  .register({
+    User: asValue(User),
+    Stage: asValue(Stage),
+    Tournament: asValue(Tournament),
+  });
 
 module.exports = container;
