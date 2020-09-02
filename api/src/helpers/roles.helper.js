ffe69b5190d3116b42dbbf408bbce9615e6d8d9f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:27:38 -0500

    [ADD] roles, status helpers added

---
+++ api/src/helpers/roles.helper.js
@@ -0,0 +1,5 @@
+// Magic string
+module.exports = {
+  PLAYER_ROLE: "PLAYER_ROLE",
+  ADMIN_ROLE: "ADMIN_ROLE",
+};
---
+++ api/src/helpers/status.helper.js
@@ -0,0 +1,5 @@
+// Magic string
+module.exports = {
+  ACTIVE: "ACTIVE",
+  INACTIVE: "INACTIVE",
+};
