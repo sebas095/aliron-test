446fde5d12577b2d9e7144807349b5ac9fedbb9f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:31:51 -0500

    [ADD] index service added

---
+++ api/src/services/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  UserService: require("./user.service"),
+  AuthService: require("./auth.service"),
+  StageService: require("./stage.service"),
+  TournamentService: require("./tournament.service"),
+};
