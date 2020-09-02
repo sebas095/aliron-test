5e5f9001b3eb28e6ce82af0635c22e52fb9efa80
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-02 03:36:00 -0500

    [ADD] user repository added

---
+++ api/src/repositories/user.repository.js
@@ -0,0 +1,20 @@
+const BaseRepository = require("./base.repository");
+const { StatusHelper } = require("../helpers");
+let _user = null;
+
+class UserRepository extends BaseRepository {
+  constructor({ User }) {
+    super(User);
+    _user = User;
+  }
+
+  async getUserByUsername(username) {
+    return await _user.findOne({ username });
+  }
+
+  async disableUser(id) {
+    return await super.update(id, { status: StatusHelper.INACTIVE });
+  }
+}
+
+module.exports = UserRepository;
