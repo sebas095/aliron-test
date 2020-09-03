ff1ebea6795f78154e44a50346c363e598003235
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-03 02:11:26 -0500

    [ADD] index helper updated

--- api/src/helpers/index.js
+++ api/src/helpers/index.js
@@ -2,4 +2,5 @@ module.exports = {
   JWTHelper: require("./jwt.helper"),
   StatusHelper: require("./status.helper"),
   RolesHelper: require("./roles.helper"),
+  MatchmakingHelper: require("./matchmaking.helper"),
 };
