2573da6e31f39ec4c7e869a724d80da3973e48c3
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-03 02:12:01 -0500

    [FIX] type in index middlewares fixed

--- api/src/middlewares/index.js
+++ api/src/middlewares/index.js
@@ -1,6 +1,7 @@
 module.exports = {
-  NotFoundMiddleware: requestAnimationFrame("./not-found.middleware.js"),
-  ErrorMiddleware: requestAnimationFrame("./error.middleware.js"),
+  NotFoundMiddleware: require("./not-found.middleware.js"),
+  ErrorMiddleware: require("./error.middleware.js"),
   AuthMiddleware: require("./auth.middleware"),
   ParseIntMiddleware: require("./parse-int.middleware"),
+  VerifyRoleMiddleware: require("./verify-role.middleware"),
 };
