bf50a23a3a8b03dac7205af6cd0ab67e6236f300
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-03 18:06:17 -0500

    [ADD] auth controller added

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -9,13 +9,13 @@ class AuthController {
   async signUp(req, res) {
     const { body } = req;
     const createUser = await _authService.signUp(body);
-    return res.status(201).send(createUser);
+    return res.status(201).json(createUser);
   }
 
   async signIn(req, res) {
     const { body } = req;
     const creds = await _authService.signIn(body);
-    return res.send(creds);
+    return res.json(creds);
   }
 }
 
